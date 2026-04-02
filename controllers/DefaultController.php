d9b2afb188a818372f9ad14314bd75733e2424eb
ItzSamdam <77008627+ItzSamdam@users.noreply.github.com>
2026-04-02 17:26:59 +0100

    feat: handling defaults, and improving router logic

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -17,4 +17,14 @@ class DefaultController
             'Access-Control-Allow-Headers' => 'Content-Type, Authorization'
         ])->setStatusCode(200);
     }
+
+    public function notFound(Request $request)
+    {
+        return Response::defaultResponse()->setHeaders([
+            'Content-Type' => 'application/json',
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers' => 'Content-Type, Authorization'
+        ])->setStatusCode(404)->setBody(['error' => 'Not Found']);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@ use Controllers\UserController;
 use Controllers\ProductController;
 use Controllers\DefaultController;
 use Middleware\AuthMiddleware;
+use Utils\Response;
 
 
 $routerPath = '/api/v1';
@@ -19,30 +20,12 @@ registerResourceRoutes($router, $routerPath . '/products', ProductController::cl
 $router->post($routerPath . '/login', [UserController::class, 'login']);
 $router->post($routerPath . '/register', [UserController::class, 'register']);
 
-// use Controllers\UserController;
-// use Controllers\ProductController;
-// use Controllers\DefaultController;
-// use Middleware\AuthMiddleware;
 
-// $routerPath = '/api/v1';
+// At the very bottom of your route file
+$router->setNotFoundHandler(function ($request) {
+    return Response::notFound("The requested endpoint/resource does not exist");
+});
 
-// $router->get('/', [DefaultController::class, 'index']);
-// $router->get($routerPath, [DefaultController::class, 'index']);
-
-// // User routes
-// $router->get($routerPath.'/users', [UserController::class, 'index']);
-// $router->get('/api/v1/users/{id}', [UserController::class, 'show']);
-// $router->post('/api/v1/users', [UserController::class, 'store']);
-// $router->put('/api/v1/users/{id}', [UserController::class, 'update'], [AuthMiddleware::class]);
-// $router->delete('/api/v1/users/{id}', [UserController::class, 'destroy'], [AuthMiddleware::class]);
-
-// // Product routes
-// $router->get('/api/v1/products', [ProductController::class, 'index']);
-// $router->get('/api/v1/products/{id}', [ProductController::class, 'show']);
-// $router->post('/api/v1/products', [ProductController::class, 'store'], [AuthMiddleware::class]);
-// $router->put('/api/v1/products/{id}', [ProductController::class, 'update'], [AuthMiddleware::class]);
-// $router->delete('/api/v1/products/{id}', [ProductController::class, 'destroy'], [AuthMiddleware::class]);
-
-// // Authentication routes
-// $router->post('/api/v1/login', [UserController::class, 'login']);
-// $router->post('/api/v1/register', [UserController::class, 'register']);
+$router->setErrorHandler(function ($exception, $request) {
+    return Response::serverError("Unexpected server error: " . $exception->getMessage());
+});
